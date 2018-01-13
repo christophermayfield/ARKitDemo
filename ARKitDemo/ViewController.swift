47875d5f23db442d70391a6d3c83f649534af955
christophermayfield <christopher.j.mayfield@gmail.com>
2018-01-12 22:28:30 -0800

    First commit

--- ARKitDemo/ViewController.swift
+++ ARKitDemo/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //world-tracking function
+    
+    //The world tracking configuration tracks the device’s orientation and position. It also detects real-world surfaces seen through the device’s camera.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    //In the viewWillDisappear(_:) method, we simply tell our AR session to stop tracking motion and processing image for the view’s content.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
     }
 
-
+   
 }
+
 
